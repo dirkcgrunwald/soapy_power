650509e5a376dcd9db39cbbec7be448d0979d86a
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2020-01-24 11:38:16 +0900

    It can read, but it have not read repeat and write yet
    It can read, but it have not read repeat and write yet
    "read" is reading from sopay_power_bin file.
    "read repeat" is hopping measure, SDRFF~.
    "write" is writing rtl_power file.

---
+++ soapypower/format_spb2rp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# convert to rtl_power format file from soapy_power_bin format file
+# $ python3 soapypower/format_spb2rp.py test_soapy_power_bin.csv converted_rtl_power.csv
+
+import sys, logging, struct, collections, io
+
+import numpy
+
+from soapypower import threadpool
+from soapypower import writer
+
+if sys.platform == 'win32':
+    import msvcrt
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    soapypowerbin_file = open(sys.argv[1], mode='rb')
+    #rtlpower_file = open(sys.argv[2], mode='w')
+    read_check_soapypowerbin_file = open(sys.argv[2], mode='wb')
+    #b = writer.BaseWriter()
+    s = writer.SoapyPowerBinFormat()
+    header, pwr_array = s.read(soapypowerbin_file)
+    r = writer.RtlPowerWriter()
+    #r.write(psd_data_or_future, time_start, time_start, samples)
+    s.write(read_check_soapypowerbin_file, header.time_start, header.time_stop, header.start, header.stop, header.step, header.samples, pwr_array)
+    read_check_soapypowerbin_file.close()
+    soapypowerbin_file.close()
+    #rtlpower_file.close()
